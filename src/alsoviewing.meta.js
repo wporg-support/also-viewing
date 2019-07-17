b7b5e0ba5f876acbbf0050ddd388c7d1d12a4632
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2019-07-18 00:05:38 +0200

    Create alsoviewing.meta.js

---
+++ src/alsoviewing.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name          WordPress.org Also Viewing
+// @namespace     http://jason.stallin.gs
+// @description   See when another person is viewing the same post.
+// @author        Clorith
+// @grant         none
+// @include       https://*.wordpress.org/support/topic/*
+// @include       https://*.wordpress.org/support/view/*
+// @include       https://wordpress.org/support/topic/*
+// @include       https://wordpress.org/support/view/*
+// @require       https://code.jquery.com/jquery-1.11.0.min.js
+// @require       https://viewing-server.herokuapp.com/socket.io/socket.io.js
+// @version       0.0.8.1
+// @updateURL     https://github.com/wporg-support/also-viewing/raw/master/src/wordpress-plugins-topic-highlighter.user.js
+// @downloadURL   https://github.com/wporg-support/also-viewing/raw/master/src/wordpress-plugins-topic-highlighter.user.js
+// ==/UserScript==

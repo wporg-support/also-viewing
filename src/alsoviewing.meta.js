b6ddef11b5c5d0a671c7e0b1d408e988c2874bff
Marius Jensen <marius@clorith.net>
2020-08-15 13:46:43 +0200

    Version bump to trigger update available for various users

--- src/alsoviewing.meta.js
+++ src/alsoviewing.meta.js
@@ -9,9 +9,9 @@
 // @include       https://wordpress.org/support/view/*
 // @require       https://code.jquery.com/jquery-1.11.0.min.js
 // @require       https://viewing-server.herokuapp.com/socket.io/socket.io.js
-// @version       0.0.8.1
+// @version       0.9.0
 // @resource      configHtml https://raw.githubusercontent.com/wporg-support/topic-highlighter/master/src/options.html
-// @updateURL     https://github.com/wporg-support/also-viewing/raw/master/src/alsoviewing.user.js
-// @downloadURL   https://github.com/wporg-support/also-viewing/raw/master/src/alsoviewing.user.js
+// @updateURL     https://github.com/wporg-support/also-viewing/raw/master/dist/alsoviewing.user.js
+// @downloadURL   https://github.com/wporg-support/also-viewing/raw/master/dist/alsoviewing.user.js
 // @grant         GM_getResourceText
 // ==/UserScript==
